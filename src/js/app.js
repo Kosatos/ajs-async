1cd23307f0d22b8783027509dcb5559964c9ab4b
Konstantin Muzyka <kosatkinms@mail.ru>
2022-03-03 00:35:13 +0300

    delete async task

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,1 @@
 import GameSavingLoader from './modules/GameSavingLoader';
-
-(async () => {
-  try {
-    const saving = await GameSavingLoader.load();
-    console.log(saving);
-  } catch (err) {
-    console.log(err);
-  }
-})();
